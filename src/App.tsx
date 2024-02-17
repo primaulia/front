ab89680b0e75294ba390b20207c5ba812f39c262
Prima Aulia Gusta <prima@tablevibe.co>
2024-02-17 15:54:12 +0800

    setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,7 @@ import { Item } from "./components/Item";
 function App() {
   return (
     <div className="App">
-      <div className="navbar">
-        <ul>
-          <li>Home</li>
-          <li>Blog</li>
-        </ul>
-      </div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Item />
     </div>
   );
